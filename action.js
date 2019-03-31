4ea28bbc7cb5816f3bd184f2f5c65b3fd0f24186
Nahla <nahlamoohamed@gmail.com>
2019-03-31 13:41:28 +0200

    Update action.js
    clear text field after submission
    Submission on enter
    New items added at first of the list
    Task timestamp added
    Editable tasks

--- action.js
+++ action.js
@@ -1,120 +1,180 @@
-//document.addEventListener('DOMContentLoaded', function(){ 
-  var Todo = function(todo, indicator){
-    this.todo = todo;
-    this.isDone = indicator;
-    this.todoConstructor = function(){
-      return `${this.todo}  ${this.isDone}`;
+var Todo = function(todo, indicator,date){
+  this.todo = todo;
+  this.isDone = indicator;
+  this.date = date;
+  this.todoConstructor = function(){
+    return `${this.todo}  ${this.isDone} ${this.date}`;
+  }
+}
+
+let toDoList = [];
+function getCurrentTime(){
+  var currentDate = new Date();
+
+  var date = currentDate.getDate();
+  var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+  var year = currentDate.getFullYear();
+  var hour = currentDate.getHours();
+  var minutes = currentDate.getMinutes();
+
+  return `${date}-${(month + 1)}-${year} ${hour}:${minutes}` ;
+}
+function appendToList(){
+  var dateString = getCurrentTime();
+  
+  var todo = new Todo(toDoListInput.value,true,dateString);
+  toDoList.unshift(todo);
+  showToDoList();
+  getActiveElements();
+  document.getElementById("toDoListInput").value='';
+}
+document.onkeydown=function(){
+  if(window.event.keyCode=='13'){
+      appendToList();
+  }
+}
+function listItemDone(item){
+  for(let i=0; i< toDoList.length;i++){
+    if(toDoList[i].todo+toDoList[i].date == item.target.parentNode.textContent){
+      toDoList.splice(i,1,new Todo(toDoList[i].todo,false,toDoList[i].date));
     }
   }
-
-  let toDoList = [];
-
-  function appendToList(){
-    var todo = new Todo(toDoListInput.value,true);
-    toDoList.push(todo);
-    showToDoList();
-    getActiveElements();
-  }
-
-  function listItemDone(item){
-    for(let i=0; i< toDoList.length;i++){
-      if(toDoList[i].todo == item.target.parentNode.textContent){
-        toDoList.splice(i,1,new Todo(item.target.parentNode.textContent,false));
-      }
+  showToDoList();
+  getActiveElements();
+}
+function deleteItem(item){
+  for(let i=0; i< toDoList.length;i++){
+    if(toDoList[i].todo+toDoList[i].date === item.target.parentNode.parentNode.parentNode.textContent){
+      toDoList.splice(i,1);
     }
-    showToDoList();
-    getActiveElements();
   }
-  function deleteItem(item){
-    for(let i=0; i< toDoList.length;i++){
-      if(toDoList[i].todo === item.target.parentNode.parentNode.textContent){
-        toDoList.splice(i,1);
-      }
-    }
-    showToDoList();
-    getActiveElements();
-  }
+  showToDoList();
+  getActiveElements();
+}
 
-  function getActiveElements(){
-    var count = 0;
-    for(let item of toDoList)
+function editItem(item){
+  for(let i=0; i< toDoList.length;i++){
+    if(toDoList[i].todo+toDoList[i].date == item.target.parentNode.textContent){
+      var promptInput = prompt("Edit!",toDoList[i].todo);
+      toDoList.splice(i,1,new Todo(promptInput,true,getCurrentTime()));
+    }
+  }
+  showToDoList();
+  getActiveElements();
+}
+
+function getActiveElements(){
+  var count = 0;
+  for(let item of toDoList)
+  {
+    if(item.isDone === true)
     {
-      if(item.isDone === true)
-      {
-        count +=1;
-      }
+      count +=1;
     }
-    //return count;
-    if(toDoList.length > 0){
-      var listLength = document.getElementById('listLength');
-      listLength.textContent = `${count} To-Do items in the list.`;
+  }
+  //return count;
+  if(toDoList.length > 0){
+    var listLength = document.getElementById('listLength');
+    listLength.textContent = `${count} To-Do items in the list.`;
+  }
+  else{
+    var listLength = document.getElementById('listLength');
+    listLength.textContent = '';
+  }
+}
+
+function showToDoList(){
+  var div = document.getElementById('toDoListDiv');
+  if(div.hasChildNodes()){
+    div.removeChild(div.firstChild);
+  }
+  var ul = document.createElement('ul');
+
+  for(let i=0; i<toDoList.length; i++){
+    if(toDoList[i].isDone == true){
+      var temp = document.createElement('li');      //ul -> li -> i -> deleteDiv -> i ->
+      temp.className = 'well todo-li';
+      temp.textContent = toDoList[i].todo;
+      var iele = document.createElement('i');
+      iele.className = 'fa fa-check-circle';
+      iele.style = 'margin-left:1%;color:black;cursor:pointer;text-decoration: none;';
+      iele.addEventListener('click',function(ev){
+        listItemDone(ev);
+      });
+      temp.appendChild(iele);
+
+      var iedit = document.createElement('i');
+      iedit.className = 'fa fa-edit';
+      iedit.style = 'margin-left:1%;color:black;cursor:pointer;text-decoration: none;';
+      iedit.addEventListener('click',function(ev){
+        editItem(ev);
+      });
+      temp.appendChild(iedit);
+      
+      var deleteDiv = document.createElement('div');
+      deleteDiv.style = 'float:right;';
+
+      var dateDiv = document.createElement('div');
+      dateDiv.style = 'display:inline-block;';
+
+      var dateLabel = document.createElement('p');
+      dateLabel.className= 'dateLabelStyle';
+      dateLabel.textContent = toDoList[i].date;
+      dateDiv.appendChild(dateLabel);
+      deleteDiv.appendChild(dateDiv);
+
+      var iDiv = document.createElement('div');
+      iDiv.style = 'display:inline-block;';
+      var i2 = document.createElement('i');
+      i2.className = 'fa fa-times-circle';
+      i2.style = 'margin-left:1%;color:red;cursor:pointer;text-decoration: none;';
+      i2.addEventListener('click',function(ev){
+        deleteItem(ev);
+      });
+      iDiv.appendChild(i2);
+      deleteDiv.appendChild(iDiv);
+      temp.appendChild(deleteDiv);
+      ul.appendChild(temp);
     }
     else{
-      var listLength = document.getElementById('listLength');
-      listLength.textContent = '';
+      var temp = document.createElement('li');
+      temp.className = 'well todo-li';
+      temp.textContent = toDoList[i].todo;
+      var lineDive = document.createElement('div');
+      lineDive.style = 'text-decoration:line-through';
+      var x = document.createElement('i');
+      x.className = 'fa fa-check-circle';
+      x.style = 'margin-left:1%;color:green;cursor: pointer;text-decoration: none;';
+      temp.appendChild(x);
+
+      var deleteDiv = document.createElement('div');
+      deleteDiv.style = 'float:right;';
+
+      var dateDiv = document.createElement('div');
+      dateDiv.style = 'display:inline-block;';
+
+      var dateLabel = document.createElement('p');
+      dateLabel.className= 'dateLabelStyle';
+      dateLabel.textContent = toDoList[i].date;
+      dateDiv.appendChild(dateLabel);
+      deleteDiv.appendChild(dateDiv);
+
+      var iDiv = document.createElement('div');
+      iDiv.style = 'display:inline-block;';
+
+      var i2 = document.createElement('i');
+      i2.className = 'fa fa-times-circle';
+      i2.style = 'margin-left:1%;color:red;cursor: pointer;text-decoration: none;';
+      i2.addEventListener('click',function(ev){
+        deleteItem(ev);
+      });
+      iDiv.appendChild(i2);
+      lineDive.appendChild(temp);
+      deleteDiv.appendChild(iDiv);
+      temp.appendChild(deleteDiv);
+      ul.appendChild(lineDive);
     }
+    div.appendChild(ul);
   }
-
-  function showToDoList(){
-    var div = document.getElementById('toDoListDiv');
-    if(div.hasChildNodes()){
-      div.removeChild(div.firstChild);
-    }
-    var ul = document.createElement('ul');
-
-    for(let i=0; i<toDoList.length; i++){
-      if(toDoList[i].isDone == true){
-        var temp = document.createElement('li');      //ul -> li -> i -> deleteDiv -> i ->
-        temp.className = 'well todo-li';
-        temp.textContent = toDoList[i].todo;
-        var iele = document.createElement('i');
-        iele.className = 'fa fa-check-circle';
-        iele.style = 'margin-left:1%;color:black;cursor:pointer;text-decoration: none;';
-        iele.addEventListener('click',function(ev){
-          listItemDone(ev);
-        });
-        temp.appendChild(iele);
-        
-        
-        var deleteDiv = document.createElement('div');
-        deleteDiv.style = 'float:right;';
-        var i2 = document.createElement('i');
-        i2.className = 'fa fa-times-circle';
-        i2.style = 'margin-left:1%;color:red;cursor:pointer;text-decoration: none;';
-        i2.addEventListener('click',function(ev){
-          deleteItem(ev);
-        });
-        deleteDiv.appendChild(i2);
-        temp.appendChild(deleteDiv);
-        ul.appendChild(temp);
-      }
-      else{
-        var temp = document.createElement('li');
-        temp.className = 'well todo-li';
-        temp.textContent = toDoList[i].todo;
-        var lineDive = document.createElement('div');
-        lineDive.style = 'text-decoration:line-through';
-        var x = document.createElement('i');
-        x.className = 'fa fa-check-circle';
-        x.style = 'margin-left:1%;color:green;cursor: pointer;text-decoration: none;';
-        temp.appendChild(x);
-        
-        var deleteDiv = document.createElement('div');
-        deleteDiv.style = 'float:right;';
-        var i2 = document.createElement('i');
-        i2.className = 'fa fa-times-circle';
-        i2.style = 'margin-left:1%;color:red;cursor: pointer;text-decoration: none;';
-        i2.addEventListener('click',function(ev){
-          deleteItem(ev);
-        });
-        lineDive.appendChild(temp);
-        deleteDiv.appendChild(i2);
-        temp.appendChild(deleteDiv);
-        ul.appendChild(lineDive);
-      }
-      div.appendChild(ul);
-    }
-    return 1;
-  }
-
-//}, false);
+  return 1;
+}
